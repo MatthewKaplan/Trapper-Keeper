2125aef5b44581f45a5fcf45ea636647969366f0
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-08-20 03:46:35 -0600

    Fix editing, send corrrect data to backend

--- src/Components/NewCard/NewCard.js
+++ src/Components/NewCard/NewCard.js
@@ -1,190 +1,168 @@
-import React, { Component } from "react";
-import { fetchAddNote } from "../../Api/fetch/fetchAddNote";
-import { fetchEditNote } from "../../Api/fetch/fetchEditNote";
-import { fetchNote } from "../../Api/fetch/fetchNote";
-import { Link } from "react-router-dom";
+import React, { Component } from 'react';
+import generate from 'shortid';
+import { fetchAddNote } from '../../Api/fetch/fetchAddNote';
+import { fetchEditNote } from '../../Api/fetch/fetchEditNote';
+import { fetchNote } from '../../Api/fetch/fetchNote';
+import { Link } from 'react-router-dom';
 
 export class NewCard extends Component {
-  state = { title: "", listItem: "", notes: [] };
+	state = { title: '', listItem: '', notes: [] };
 
-  componentDidMount() {
-    this.grabInfo(this.props.id);
-  }
+	componentDidMount () {
+		this.grabInfo(this.props._id);
+	}
 
-  grabInfo = async (id) => {
-    if (id) {
-      const response = await fetchNote(id);
-      const { title, list } = response;
+	grabInfo = async id => {
+		if (id) {
+			const response = await fetchNote(id);
+			const { title, notes } = response;
+			this.setState({
+				title,
+				notes: notes
+			});
+		}
+	};
 
-      this.setState({
-        title,
-        notes: list
-      });
-    }
-  };
+	handleChange = event => {
+		const { name, value } = event.target;
 
-  handleChange = event => {
-    const { name, value } = event.target;
+		this.setState({ [name]: value });
+	};
 
-    this.setState({ [name]: value });
-  };
+	handleComplete = event => {
+		const { notes } = this.state;
 
-  handleComplete = event => {
-    const { notes } = this.state;
-    let { id } = event.target;
+		const togglecomplete = notes.find(note => note._id === event);
+		togglecomplete.completed = !togglecomplete.completed;
 
-    id = parseInt(id);
+		this.setState({ notes });
+	};
 
-    const togglecomplete = notes.find(note => note.id === id);
-    togglecomplete.completed = !togglecomplete.completed;
+	handleKeyPress = () => {
+		const { listItem, notes } = this.state;
+		const newNote = {
+			message: listItem,
+			completed: false
+		};
 
-    this.setState({ notes });
-  };
+		this.setState({
+			notes: [ ...notes, newNote ],
+			listItem: ''
+		});
+	};
 
-  handleKeyPress = () => {
-    console.log('Keypress')
-    const { listItem, notes } = this.state;
-    const newNote = {
-      message: listItem,
-      completed: false
-    };
+	handleSaveNote = id => {
+		const { title, notes } = this.state;
+		if (id) {
+			const note = { _id: id, title, notes };
+			const updated = { title, notes };
+			fetchEditNote(note, updated);
+		} else {
+			fetchAddNote(title, notes);
+		}
+	};
 
-    this.setState({
-      notes: [...notes, newNote],
-      listItem: ""
-    });
-  };
+	deleteListItem = noteId => {
+		const notes = this.state.notes.filter(note => note._id !== noteId);
+		this.setState({ notes });
+	};
 
-  handleSaveNote = (id) => {
-    const { title, notes } = this.state;
-    if (id) {
-      const updatedList = { id, title, notes };
-      fetchEditNote(updatedList);
-    } else {
-      fetchAddNote(title, notes);
-    }
-  };
+	editListItem = (noteId, e) => {
+		const { notes } = this.state;
+		const note = notes.find(n => n._id === noteId);
+		note.message = e.target.value;
+		this.setState({ notes });
+	};
 
-  deleteListItem = noteId => {
-    const notes = this.state.notes.filter(note => note.id !== noteId);
-    this.setState({ notes });
-  };
+	render () {
+		const { notes } = this.state;
+		let todos = notes.filter(note => !note.completed).map(incomplete => {
+			return (
+				<section key={generate()} className="note-item-component">
+					<div className="unchecked" id={incomplete._id} onClick={() => this.handleComplete(incomplete._id)} />
+					<input
+						className="todo"
+						data-test="todo"
+						value={incomplete.message}
+						onChange={e => this.editListItem(incomplete._id, e)}
+					/>
+					<i
+						className="material-icons delete"
+						data-test="delete-button-incomplete"
+						onClick={() => this.deleteListItem(incomplete._id)}>
+						delete_forever
+					</i>
+				</section>
+			);
+		});
 
-  editListItem = (noteId, e) => {
-    const { notes } = this.state;
-    const note = notes.find(n => n.id === noteId);
-    note.message = e.target.value;
-    this.setState({ notes });
-  };
+		let complete = notes.filter(note => note.completed).map(complete => {
+			return (
+				<section key={complete.id} className="note-item-component">
+					<div className="checked" id={complete._id} onClick={() => this.handleComplete(complete._id)} />
+					<input
+						className="completed todo"
+						data-test="completed"
+						value={complete.message}
+						onChange={e => this.editListItem(complete._id, e)}
+					/>
+					<i
+						className="material-icons"
+						data-test="delete-button-complete"
+						onClick={() => this.deleteListItem(complete._id)}>
+						delete_forever
+					</i>
+				</section>
+			);
+		});
 
-  render() {
-    const { notes } = this.state;
-
-    let todos = notes
-      .filter(note => !note.completed)
-      .map(incomplete => {
-        return (
-          <section key={incomplete.id} className="note-item-component">
-            <div
-              className="unchecked"
-              id={incomplete.id}
-              onClick={this.handleComplete}
-            />
-            <input
-              className="todo"
-              data-test='todo'
-              value={incomplete.message}
-              onChange={e => this.editListItem(incomplete.id, e)}
-            />
-            <i
-              className="material-icons delete"
-              data-test="delete-button-incomplete"
-              onClick={() => this.deleteListItem(incomplete.id)}
-            >
-              delete_forever
-            </i>
-          </section>
-        );
-      });
-
-    let complete = notes
-      .filter(note => note.completed)
-      .map(complete => {
-        return (
-          <section key={complete.id} className="note-item-component">
-            <div
-              className="checked"
-              id={complete.id}
-              onClick={this.handleComplete}
-            />
-            <input
-              className="completed todo"
-              data-test='completed'
-              value={complete.message}
-              onChange={e => this.editListItem(complete.id, e)}
-            />
-            <i
-              className="material-icons"
-              data-test="delete-button-complete"
-              onClick={() => this.deleteListItem(complete.id)}
-            >
-              delete_forever
-            </i>
-          </section>
-        );
-      });
-
-    return (
-      <div className="new-card-component">
-        <form className="new-card-form">
-          <input
-            id="title"
-            type="text"
-            name="title"
-            className="note-title"
-            value={this.state.title}
-            placeholder="Title"
-            onChange={(e) => this.handleChange(e)}
-          />
-          <section className="add-new-note">
-            <div className="plus" />
-            <input
-              type="text"
-              name="listItem"
-              className="list-item"
-              value={this.state.listItem}
-              placeholder="List Item"
-              data-test="enter-item"
-              onChange={(e) => this.handleChange(e)}
-              onKeyPress={event => {
-                if (event.key === "Enter"){
-                  this.handleKeyPress();
-                }
-              }}
-            />
-          </section>
-        </form>
-        {todos}
-        {complete}
-        {this.state.notes.length ? (
-          <section className="new-note-btns">
-            <button
-              className="save-note"
-              data-test="save-note"
-              onClick={() => this.handleSaveNote(this.props.id)}
-            >
-              Save note
-            </button>
-            <Link to={"/"}>
-              <button className="save-note" data-test="return-to-notes">
-                Return to all notes
-              </button>
-            </Link>
-          </section>
-        ) : null}
-      </div>
-    );
-  }
+		return (
+			<div className="new-card-component">
+				<form className="new-card-form">
+					<input
+						id="title"
+						type="text"
+						name="title"
+						className="note-title"
+						value={this.state.title}
+						placeholder="Title"
+						onChange={e => this.handleChange(e)}
+					/>
+					<section className="add-new-note">
+						<div className="plus" />
+						<input
+							type="text"
+							name="listItem"
+							className="list-item"
+							value={this.state.listItem}
+							placeholder="List Item"
+							data-test="enter-item"
+							onChange={e => this.handleChange(e)}
+							onKeyPress={event => {
+								if (event.key === 'Enter') {
+									this.handleKeyPress();
+								}
+							}}
+						/>
+					</section>
+				</form>
+				{todos}
+				{complete}
+				{this.state.notes.length ? (
+					<section className="new-note-btns">
+						<button className="save-note" data-test="save-note" onClick={() => this.handleSaveNote(this.props._id)}>
+							Save note
+						</button>
+						<Link to={'/'}>
+							<button className="save-note" data-test="return-to-notes">
+								Return to all notes
+							</button>
+						</Link>
+					</section>
+				) : null}
+			</div>
+		);
+	}
 }
 
 export default NewCard;
